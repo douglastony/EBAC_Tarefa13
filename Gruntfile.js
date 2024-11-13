5072a8e07e3c9c75ca80ed2214d80a765436d6ff
douglastony <douglasjorge.github@gmail.com>
2024-11-13 20:40:11 -0300

    Commit para corrigir pequeno errro de digitação

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,7 +117,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-Uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-replace');
